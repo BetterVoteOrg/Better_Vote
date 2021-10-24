1b8002888d496d80cd2d24d1697faae2aae3c700
Aubrey <60026978+au6rey@users.noreply.github.com>
2021-10-24 14:11:13 -0500

    .

--- lib/main.dart
+++ lib/main.dart
@@ -62,7 +62,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Authentication Demo',
+      title: 'BetterVote',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
