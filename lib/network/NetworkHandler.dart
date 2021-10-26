4f338a80a9cb5fc70f44109f151818ba505da60a
Aubrey <60026978+au6rey@users.noreply.github.com>
2021-10-25 22:44:45 -0500

    Update apiHost

--- lib/network/NetworkHandler.dart
+++ lib/network/NetworkHandler.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 class NetworkHandler {
   NetworkHandler(this._path);
   String _path;
-  final String _apiHost = "https://bettervote.herokuapp.com/api";
+  final String _apiHost = "https://bettervote.herokuapp.com";
   // final String _apiHost =
   //     "https://5a09-2600-8807-305-3100-d5ba-7a2f-9541-c218.ngrok.io/api";
 
