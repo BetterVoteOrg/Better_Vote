a8e2ff1d58969b883a764f95e5ffbc8252b7dff5
Aubrey <60026978+au6rey@users.noreply.github.com>
2021-11-03 15:20:08 -0500

    Fix create a poll form

--- lib/views/tabs/CreatePollTab.dart
+++ lib/views/tabs/CreatePollTab.dart
@@ -1,25 +1,27 @@
 import 'package:better_vote/controllers/UserController.dart';
+import 'package:better_vote/views/tabs/forms/CreateAPollForm.dart';
 import 'package:flutter/material.dart';
 
-class CreatePollPage extends StatefulWidget {
-  CreatePollPage({Key key}) : super(key: key);
+class CreatePollTabPage extends StatefulWidget {
+  CreatePollTabPage({Key key}) : super(key: key);
   @override
-  State<CreatePollPage> createState() => CreatePollState();
+  State<CreatePollTabPage> createState() => CreatePollState();
 }
 
-class CreatePollState extends State<CreatePollPage> {
+class CreatePollState extends State<CreatePollTabPage> {
   final user = User();
   @override
   Widget build(BuildContext context) {
-    const TextStyle optionStyle =
-        TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-    return Scaffold(
-        body: Center(
-      child: Text(
-        'Index 2: Create Poll',
-        style: optionStyle,
-      ),
-    )
+    // const TextStyle optionStyle =
+    //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    return Scaffold(body: CreateAPollForm()
+
+        //     Center(
+        //   child: Text(
+        //     'Index 2: Create Poll',
+        //     style: optionStyle,
+        //   ),
+        // )
         // FutureBuilder(
         //     builder: profileBuilder, future: user.canFindProfileData()),
         );
