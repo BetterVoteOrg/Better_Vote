1b00f9e965bd2b80a31d2f8b4c731014d1328e38
Bryce-Lee <brycelee31@yahoo.com>
2021-11-09 00:24:25 -0600

    Created Profile page

--- lib/views/tabs/ProfileTab.dart
+++ lib/views/tabs/ProfileTab.dart
@@ -23,23 +23,146 @@ class ProfileState extends State<ProfileTabPage> {
   }
 
   Widget profileBuilder(BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+    const darkGreen = const Color(0xFF008037);
+    String dropdownValue = 'Active';
     if (snapshot.hasData) {
       User _user = snapshot.data;
-      return Center(
-        child: Column(
-          children: [
-            Text("WELCOME"),
-            Text("Username:  " + _user.getUsername()),
-            Text("Email: " + _user.getEmail()),
-            Text("Account created: " + _user.getCreatedAt()),
-          ],
-        ),
-      );
+      return Scaffold(
+          body: Column(
+        children: [
+          Container(
+            //Top User Info Bar
+            alignment: Alignment.topCenter,
+            color: darkGreen,
+            height: 275,
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                // 2 Top Buttons
+                children: [
+                  IconButton(
+                      alignment: Alignment.topLeft,
+                      onPressed: onPressed,
+                      icon: Icon(Icons.arrow_back, size: 50)),
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          primary: Colors.black,
+                          padding: EdgeInsets.fromLTRB(0, 15, 15, 0),
+                          textStyle: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.bold)),
+                      onPressed: onPressed,
+                      child: Text("EDIT"))
+                ],
+              ),
+              Icon(Icons.account_circle_outlined, size: 150),
+              Text(
+                _user.getUsername(),
+                style: const TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 2,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Text("# Polls Created: ", textScaleFactor: 1.1),
+                  Text("# Polls Participated in: ", textScaleFactor: 1.1)
+                ]),
+              ),
+            ]),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: darkGreen,
+                        border: Border(
+                            top: BorderSide(width: 4, color: Color(0xFF000000)),
+                            left:
+                                BorderSide(width: 0, color: Color(0xFF000000)),
+                            right:
+                                BorderSide(width: 3, color: Color(0xFF000000)),
+                            bottom: BorderSide(
+                                width: 4, color: Color(0xFF000000)))),
+                    child: TextButton(
+                        style: TextButton.styleFrom(
+                            primary: Colors.black,
+                            padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
+                            textStyle: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold)),
+                        onPressed: onPressed,
+                        child: Text("Polls I created"))),
+              ),
+              Expanded(
+                  child: Container(
+                      decoration: BoxDecoration(
+                          color: darkGreen,
+                          border: Border(
+                              top: BorderSide(
+                                  width: 4, color: Color(0xFF000000)),
+                              left: BorderSide(
+                                  width: 3, color: Color(0xFF000000)),
+                              right: BorderSide(
+                                  width: 0, color: Color(0xFF000000)),
+                              bottom: BorderSide(
+                                  width: 4, color: Color(0xFF000000)))),
+                      child: TextButton(
+                          style: TextButton.styleFrom(
+                              primary: Colors.black,
+                              padding: EdgeInsets.fromLTRB(0, 0, 25, 0),
+                              textStyle: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.bold)),
+                          onPressed: onPressed,
+                          child: Text("Polls I voted in"))))
+            ],
+          ),
+          Container(
+            height: 50,
+            padding: EdgeInsets.fromLTRB(10, 0, 0, 0),
+            alignment: Alignment.topLeft,
+            child: Row(children: [
+              Text("Sort By: ",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 18)),
+              DropdownButton<String>(
+                value: dropdownValue,
+                onChanged: (value) {
+                  setState(() {
+                    dropdownValue = value;
+                    print(dropdownValue);
+                  });
+                },
+                items: <String>['Active', 'Closed', 'Third']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value,
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black)));
+                }).toList(),
+                underline: Container(
+                  height: 1,
+                  color: Colors.deepPurpleAccent,
+                ),
+              ),
+            ]),
+          ), //Sort by dropdown
+          Column(
+              //ONLY THING LEFT TO IMPLEMENT
+              ) // Polls
+        ],
+      ));
     }
     if (snapshot.hasError) return Text("An error occurred fetching user data.");
 
     return Center(
       child: CircularProgressIndicator(),
     );
+  }
+
+  void onPressed() {
+    print("Tried to go back");
   }
 }
